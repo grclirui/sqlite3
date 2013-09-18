118e9daebcce75ad7998e2d78071c5140f2a3bce
Rui Li <li.rui@ge.com>
2013-09-18 00:48:40 -0400

    correct cmake typo

--- Generatesqlite3Config.cmake
+++ Generatesqlite3Config.cmake
@@ -2,25 +2,25 @@
 
 # Settings specific to the build tree
 #
-set(SQLITE3_INCLUDE_DIR_CONFIG
-  ${SQLITE3_INCLUDE_DIR}
+set(SQLITE_INCLUDE_DIR_CONFIG
+  ${SQLITE_INCLUDE_DIR}
   )
 
-set(SQLITE3_LIBRARIES_CONFIG sqlite3)
+set(SQLITE_LIBRARIES_CONFIG sqlite3)
 
 #set(SQLITE3_LIBRARY_DIR_CONFIG
 #  ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
 #  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
 #  )
 
-set(SQLITE3_USE_FILE_CONFIG
-  ${SQLITE3_BINARY_DIR}/Usesqlite3.cmake
+set(SQLITE_USE_FILE_CONFIG
+  ${SQLITE_BINARY_DIR}/Usesqlite3.cmake
   )
 
 # configure PythonCppApiConfig.cmake for the build tree
 #
 configure_file(
-  ${SQLITE3_SOURCE_DIR}/sqlite3Config.cmake.in
-  ${SQLITE3_BINARY_DIR}/sqlite3Config.cmake
+  ${SQLITE_SOURCE_DIR}/sqlite3Config.cmake.in
+  ${SQLITE_BINARY_DIR}/sqlite3Config.cmake
   @ONLY
   )
